39139d6a15d074d04ee40f5680a2ff637e1b3c2a
marga <margarita.naranjo@gmail.com>
2024-09-26 21:23:11 +0200

    Solución Iteración 1

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
+const hacker1 = "Antonio";
+console.log (`The diver's name is ${hacker1}`);
 
+const hacker2 = "Tito";
+console.log (`The navigator´s name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
