adf2d12c7d69428526e7bf05af76ad9533316eb9
marga <margarita.naranjo@gmail.com>
2024-09-27 18:45:34 +0200

    Solución Iteración 3

--- index.js
+++ index.js
@@ -31,4 +31,28 @@ for (let i = countLetras2; i >= 0 ; i--) {
 
 console.log(arrayReves.join(''));
 
+/* voy por el punto 3.3  y he hecho push de todo */
 
+
+const abece = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let indice1 = 0;
+let indice2 = 0;
+const letraH1 = hacker1.split('')[0];
+const letraH2 = hacker2.split('')[0];
+
+for (i = 0; i < abece.length; i++) {
+    
+    if (letraH1.toLowerCase() === abece[i]) {        
+        indice1 = i;
+    } else if (letraH2.toLowerCase() === abece[i]) {
+        indice2 = i;
+    }
+}
+
+if (indice1 > indice2) {
+    console.log (`The driver's name goes first.`);
+} else if (indice1 < indice2) {
+    console.log (`Yo, the navigator goes first, definitely.`);
+} else {
+    console.log (`What?! You both have the same name?`);
+}
